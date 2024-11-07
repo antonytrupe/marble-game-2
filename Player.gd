f2f4ef7de611b954d2cc34428a500d252200bbd5
TonyT <antony.trupe@gmail.com>
2024-11-07 11:14:19 -0500

    jump height

--- Player.gd
+++ Player.gd
@@ -13,7 +13,7 @@ signal health_changed(health_value)
 @export var player_id:String
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 10.0
+const JUMP_VELOCITY = 14.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 20.0
