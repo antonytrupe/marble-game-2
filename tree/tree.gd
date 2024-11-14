c23e1a04d60883c0c4c47e2ccdeadb8455380ddf
TonyT <antony.trupe@gmail.com>
2024-11-14 16:17:25 -0500

    made trees scale with age

---
+++ tree/tree.gd
@@ -0,0 +1,40 @@
+#@tool
+extends Node3D
+
+@export var maturity: int = 1000 * 60 * 60 * 24 * 360 * 10
+
+@export var birth_date: int = 0:
+	set = set_birth_date
+@export var extra_age: int = 0:
+	set = set_extra_age
+
+@export var world: Node3D
+
+var calculated_age: int:
+	get = calculate_age
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#rotation.y += PI * delta * .1
+	var s = clampf(float(calculated_age) / maturity, .1, 1.0)
+	#s = .5
+	#print(s)
+	scale = Vector3(s, s, s)
+
+
+func set_birth_date(value):
+	birth_date = value
+
+
+func set_extra_age(value):
+	extra_age = value
+
+
+func calculate_age():
+	return extra_age
