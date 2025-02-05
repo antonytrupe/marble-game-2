7a3aad66a17a4cec7e44e9ab15bb3271ee217a25
TonyT <antony.trupe@gmail.com>
2025-02-05 18:13:28 -0500

    window bar thing

--- window_bar.gd
+++ window_bar.gd
@@ -1,2 +1,30 @@
 class_name WindowBar
 extends Panel
+
+@export var game:Game
+
+
+func _unhandled_input(event):
+	print('window bar _unhandled_input')
+	game._unhandled_input(event)
+
+
+func _on_warp_button_pressed() -> void:
+	var event = InputEventAction.new()
+	event.action = "warp_vote"
+	event.pressed = true
+	Input.parse_input_event(event)
+
+
+func _on_craft_button_pressed() -> void:
+	var event = InputEventAction.new()
+	event.action = "craft"
+	event.pressed = true
+	Input.parse_input_event(event)
+
+
+func _on_inventory_button_pressed() -> void:
+	var event = InputEventAction.new()
+	event.action = "inventory"
+	event.pressed = true
+	Input.parse_input_event(event)
